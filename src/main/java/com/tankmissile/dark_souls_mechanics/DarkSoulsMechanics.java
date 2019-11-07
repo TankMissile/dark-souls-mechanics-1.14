8fa7b47091e7330ac8c12a5d2989c83126f9d0a6
TankMissile <trevans1@yahoo.com>
2019-11-06 23:54:07 -0600

    set up mod info

---
+++ src/main/java/com/tankmissile/dark_souls_mechanics/DarkSoulsMechanics.java
@@ -0,0 +1,8 @@
+package com.tankmissile.dark_souls_mechanics;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DarkSoulsMechanics.MOD_ID)
+public class DarkSoulsMechanics {
+    public static final String MOD_ID = "dark_souls_mechanics";
+}
